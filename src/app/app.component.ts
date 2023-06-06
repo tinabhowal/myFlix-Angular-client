911c335b3701b6f225ad208d385bf11ecffa0df0
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-06-06 15:01:09 +0200

    task 6.3 login and signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'myFlix-Angular-client';
+// }
+
+
+// src/app/app.component.ts
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +23,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+// This is the function that will open the dialog when the signup button is clicked  
+openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
+
+openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+// Assigning the dialog a width
+    width: '280px'
+    });
+  }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,60 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user-login-form',
+//   templateUrl: './user-login-form.component.html',
+//   styleUrls: ['./user-login-form.component.scss']
+// })
+// export class UserLoginFormComponent {
+
+// }
+
+
+
+// src/app/user-registration-form/user-login-form.component.ts
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
+}
+
+// This is the function responsible for sending the form inputs to the backend
+loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+  // Logic for a successful user registration goes here! (To be implemented)
+  console.log(result);  
+  this.dialogRef.close(); // This will close the modal on success!
+     this.snackBar.open("Logged in", 'OK', {
+        duration: 2000
+     });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  }
